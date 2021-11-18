203e9bc60ae01005d70b37368a7f42deed4ab3a4
Lenny Erlesand <lenny@erlesand.se>
2021-11-18 08:17:40 +0100

    Day 1

--- src/todo/App.jsx
+++ src/todo/App.jsx
@@ -1,7 +1,15 @@
-const initialTodos = require('./todos.json');
+import Todo from "./Todo";
+import TodoForm from "./TodoForm";
 
-// ...
+const initialTodos = require("./todos.json");
 
 export const App = () => {
-  return null;
-}
+  return (
+    <>
+      <TodoForm todos={initialTodos} />
+      {initialTodos?.map(({ userId, ...todo }) => (
+        <Todo key={todo.id} {...todo} />
+      ))}
+    </>
+  );
+};
